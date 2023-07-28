c03f93bcb1fde7224eda231b8297c85fda533ad9
Jeehyeok Kim <jk13@nate.com>
2023-07-28 13:33:41 +0900

    code refactoring

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from google.cloud import retail_v2
+import sys
+PROJECT_ID = sys.argv[1]
 
 st.set_page_config(page_title='Recommendations AI test', 
                     page_icon=None, 
@@ -49,15 +51,12 @@ def init_services():
     get_prediction_service()
     get_search_service()
     get_userevent_service()
+    st.session_state.recommendations = {}
+    st.session_state.clicked_product_id = None
     return pd.read_csv("movies.csv")
 
 movies_df = init_services()
-
-col1, col2 = st.columns([2, 1])
-searchtxt = col1.text_input("Search", "", key="txtsearch")
-if len(searchtxt) == 0:
-    searchtxt = None
-userid = col2.text_input("UserId", "visitor1")
+view_size = 8
 
 def get_movie_title(product_id):
     return movies_df[movies_df['movieId'] == int(product_id)][['title']].values[0][0]
@@ -75,7 +74,7 @@ def get_predict(model, event_type, visitor_id, num, product_id = None, validate_
         user_event.product_details = [product_detail]
 
     request = retail_v2.PredictRequest(
-        placement=f"projects/sandbox-373102/locations/global/catalogs/default_catalog/servingConfigs/{model}",
+        placement=f"projects/{PROJECT_ID}/locations/global/catalogs/default_catalog/servingConfigs/{model}",
         user_event=user_event,
         page_size=num,
         validate_only=validate_only,
@@ -83,7 +82,6 @@ def get_predict(model, event_type, visitor_id, num, product_id = None, validate_
     )
     # Make the request
     response = get_prediction_service().predict(request=request)
-    #print(response)
     # Handle the response
     return list(map(lambda x : x.id, response.results))
 
@@ -106,7 +104,7 @@ def update_userevent(view, visitor_id, product_id, searchtxt):
         user_event.product_details = [product_detail]
 
     request = retail_v2.WriteUserEventRequest(
-        parent=f"projects/sandbox-373102/locations/global/catalogs/default_catalog",
+        parent=f"projects/{PROJECT_ID}/locations/global/catalogs/default_catalog",
         user_event=user_event,
     )
     get_userevent_service().write_user_event(request=request)
@@ -115,7 +113,7 @@ def get_search(model, query, num, visitor_id):
     spec = retail_v2.SearchRequest.PersonalizationSpec()
     spec.mode = retail_v2.SearchRequest.PersonalizationSpec.Mode.AUTO
     request = retail_v2.SearchRequest(
-        placement=f"projects/sandbox-373102/locations/global/catalogs/default_catalog/servingConfigs/{model}",
+        placement=f"projects/{PROJECT_ID}/locations/global/catalogs/default_catalog/servingConfigs/{model}",
         query=query,
         page_size=num,
         visitor_id=visitor_id
@@ -123,91 +121,56 @@ def get_search(model, query, num, visitor_id):
     response = get_search_service().search(request=request)
     return list(map(lambda x : x.id, response.results))
 
-if 'userid' not in st.session_state:
-    st.session_state['userid'] = userid
-elif st.session_state['userid'] != userid:
-    #clear cache and refresh
-    st.session_state['userid'] = userid
-    st.session_state.pop('liked_product_id', None)
-    st.session_state.pop('prediction_results', None)
-    st.experimental_rerun()
+def on_search_change():
+    if len(st.session_state.search) > 0:
+        st.session_state.recommendations.clear()
+        st.session_state.recommendations["personalized-search"] = get_search(
+            "personalized-search", st.session_state.search, view_size, st.session_state.userid)
+    
+def on_user_change():
+    if len(st.session_state.userid) > 0:
+        st.session_state.recommendations.clear()
 
-def get_whole_recommends(clicked_item, max_num):
+col1, col2 = st.columns([2, 1])
+col1.text_input("Search", "", key="search", on_change=on_search_change)
+col2.text_input("UserId", "visitor1", key="userid", on_change=on_user_change)
+
+if not st.session_state.recommendations:
+    #Get initial recommends
+    st.session_state.recommendations["movielens-recommendation"] = get_predict(
+        "movielens-recommendation", "home-page-view", st.session_state.userid, view_size)
+
+def get_whole_recommends(clicked_view, clicked_item, max_num):
     whole_recommends = {}
-    view = "detail-page-view"
-    recommends = get_predict("movielens-pageoptimization", view, userid, max_num, clicked_item)
+    recommends = get_predict("movielens-pageoptimization", clicked_view, st.session_state.userid, max_num, clicked_item)
     for model in recommends:
-        whole_recommends[model] = get_predict(model, view, userid, max_num, clicked_item)
+        whole_recommends[model] = get_predict(model, clicked_view, st.session_state.userid, max_num, clicked_item)
     return whole_recommends
 
-view_size = 8
-canvas = st.empty()
-container = canvas.container()
-def render_view(model, view, view_size, liked_product_id, recommends):
-    #print(f"Start rendering {model}")
-    recommend = None
+def on_item_click(clicked_view, clicked_product_id):
+    st.session_state.clicked_product_id = clicked_product_id
+    st.session_state.recommendations = get_whole_recommends(clicked_view, clicked_product_id, view_size)
+    update_userevent(clicked_view, st.session_state.userid, clicked_product_id, None)
+
+container = st.container()
+def render_view(model, view, clicked_product_id, recommends):
     match model:
         case "movielens-recommendation":
             #Basic recommend
             container.write("Recommended for you from your entire history")
         case "movielens-others-you-may-like":
             #Others you may like
-            container.write(f"Others you may like bacause you liked '{get_movie_title(liked_product_id)}'")
+            container.write(f"Others you may like bacause you liked '{get_movie_title(clicked_product_id)}'")
         case "movielens-similar":
             #Similar items
-            container.write(f"Similar items like '{get_movie_title(liked_product_id)}'")
+            container.write(f"Similar items like '{get_movie_title(clicked_product_id)}'")
         case "personalized-search":
             container.write("Search results by your preferences")
-    if view_size > len(recommends):
-        view_size = len(recommends)
-    cols = container.columns([1 for _ in range(view_size)])
+    cols = container.columns([1 for _ in range(len(recommends))])
     for idx, col in enumerate(cols):
-        if col.button(get_movie_title(recommends[idx]), key=model+recommends[idx]):
-            recommend = recommends[idx]
-    return view, recommend
+        col.button(get_movie_title(recommends[idx]), key=model+recommends[idx], on_click=on_item_click, args=[view, recommends[idx]])
 
-if 'searchtxt' not in st.session_state:
-    st.session_state['searchtxt'] = searchtxt
-elif st.session_state['searchtxt'] == searchtxt:
-    searchtxt = None    
-
-clicked_view = None
-clicked_product_id = None
-if searchtxt is not None:
-    #Search rendering
-    print("####Start rendering searched item")    
-    recommends = get_search("personalized-search", searchtxt, view_size, userid)
-    if len(recommends) > 0:
-        clicked_view, clicked_product_id = render_view("personalized-search", "search", view_size, None, recommends)
-        if clicked_product_id is not None:
-            st.session_state['searchtxt'] = searchtxt
-    else:
-        st.session_state['searchtxt'] = searchtxt
-        container.write(f"No search result")
-elif "prediction_results" not in st.session_state:
-#elif st.session_state.isinit == False:
-#    st.session_state.isinit = True
-    #Initial rendering, use basic recommend only
-    print("##Start Initial rendering")
-    container.write(f"Hello {userid}!")
-    recommends = get_predict("movielens-recommendation", "home-page-view", userid, view_size)
-    clicked_view, clicked_product_id = render_view("movielens-recommendation", "home-page-view", view_size, None, recommends)
-    print(f"{clicked_view} / {clicked_product_id}")
-else:
-    #Rendering cached item which data processed from last page
-    print("#####Start rendering cached item")
-    data_to_render = st.session_state['prediction_results']
-    for model, recommends in data_to_render.items():
-        a, b = render_view(model, "detail-page-view", view_size, st.session_state['liked_product_id'], recommends)
-        if b is not None:
-            clicked_view, clicked_product_id = a, b
-
-if clicked_product_id is not None:
-    print("Clicked!!!")
-    canvas.empty()
-    st.session_state['liked_product_id'] = clicked_product_id
-    st.session_state['prediction_results'] = get_whole_recommends(clicked_product_id, view_size)
-    update_userevent(clicked_view, userid, clicked_product_id, searchtxt)
-    st.experimental_rerun()
-
-print("Rendering complete")
+#Rendering cached items
+for model, recommends in st.session_state.recommendations.items():
+    #Rendering items
+    render_view(model, "detail-page-view", st.session_state.clicked_product_id, recommends)
